6da26343010c32ee60f6b78f3a4a4e284ac51fe7
Adam Waselnuk <awaselnuk@mygrocerydeals.com>
2014-12-10 09:18:45 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Chat App Exercise
+
+The chat app exercise from [http://socket.io/get-started/chat/](http://socket.io/get-started/chat/).
---
+++ index.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+// EXPRESS ROUTE HANDLER
+// .get gets an Express application setting
+// request and response are the node objects
+// with additional stuff added by Express
+app.get('/', function(request, response){
+  response.sendFile(__dirname + '/index.html');
+});
+
+// Setting up static files with Express
+app.use('/styles', express.static(__dirname + '/styles'));
+
+// Notify when an IO connection is accomplished
+io.on('connection', function(socket){
+  console.log('a user connected');
+
+  // Notify when an IO connection is terminated
+  socket.on('disconnect', function(){
+    console.log('a user disconnected');
+  });
+
+  // Notify when a chat message is received
+  socket.on('chat message', function(msg){
+    console.log('chat message received: ' + msg);
+  });
+});
+
+// Fire up a local server
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
